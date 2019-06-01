c26afcc0abd17a913d5cea3143e248b23fd6f559
Samu Lang <langsamu@hotmail.com>
2019-06-01 21:17:30 +0200

    Moves stub implementation to controller.

---
+++ ShaclService/Controllers/DefaultController.cs
@@ -0,0 +1,31 @@
+﻿namespace ShaclService.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.IO;
+    using VDS.RDF;
+    using VDS.RDF.Parsing;
+    using VDS.RDF.Shacl;
+    using VDS.RDF.Writing;
+
+    public class DefaultController : ControllerBase
+    {
+        [HttpPost("")]
+        public void Default()
+        {
+            var shaclGraph = new Graph();
+
+            using (var reader = new StreamReader(Request.Body))
+            {
+                new TurtleParser().Load(shaclGraph, reader);
+            }
+
+            var shapesGraph = new ShapesGraph(shaclGraph);
+            var report = shapesGraph.Validate(shaclGraph).Normalised;
+
+            using (var writer = new StreamWriter(Response.Body))
+            {
+                new CompressingTurtleWriter().Save(report, writer);
+            }
+        }
+    }
+}
--- ShaclService/Startup.cs
+++ ShaclService/Startup.cs
@@ -3,16 +3,12 @@
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.DependencyInjection;
-    using System.IO;
-    using VDS.RDF;
-    using VDS.RDF.Parsing;
-    using VDS.RDF.Shacl;
-    using VDS.RDF.Writing;
 
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -22,23 +18,7 @@
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                var shaclGraph = new Graph();
-
-                using (var reader = new StreamReader(context.Request.Body))
-                {
-                    new TurtleParser().Load(shaclGraph, reader);
-                }
-
-                var shapesGraph = new ShapesGraph(shaclGraph);
-                var report = shapesGraph.Validate(shaclGraph).Normalised;
-
-                using (var writer = new StreamWriter(context.Response.Body))
-                {
-                    new CompressingTurtleWriter().Save(report, writer);
-                }
-            });
+            app.UseMvc();
         }
     }
 }
