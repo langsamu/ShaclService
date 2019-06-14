eeb5c6659b60830416f28856fb85854dd6df4ae7
Samu Lang <langsamu@hotmail.com>
2019-06-14 19:24:16 +0100

    Adds CSV rendering for SHACL validation report.

--- ShaclService/Models/Configuration.cs
+++ ShaclService/Models/Configuration.cs
@@ -5,6 +5,7 @@
     using System.IO;
     using VDS.RDF;
     using VDS.RDF.Parsing;
+    using VDS.RDF.Query;
     using VDS.RDF.Writing;
 
     public static class Configuration
@@ -17,6 +18,29 @@
             ("application/ld+json", "json", (g, reader) => new JsonLdParser().Load(g.AsTripleStore(), reader), (g, writer) => new JsonLdWriter().Save(g.AsTripleStore(), writer)),
             ("application/rdf+xml", "xml", (g, reader) => new RdfXmlParser().Load(g, reader), (g, writer) => new RdfXmlWriter(WriterCompressionLevel.High, false).Save(g, writer)),
             ("application/rdf+json", "rj", (g, reader) => new RdfJsonParser().Load(g, reader), (g, writer) => new RdfJsonWriter().Save(g, writer)),
+            ("text/csv", "csv", null, (g, writer) => {
+                var results = (SparqlResultSet)g.ExecuteQuery($@"
+PREFIX sh: <{VDS.RDF.Shacl.Vocabulary.BaseUri}>
+
+SELECT ?focusNode ?resultPath ?value ?sourceShape ?sourceConstraint ?sourceConstraintComponent ?resultSeverity ?resultMessage
+WHERE {{
+    ?result
+        a sh:ValidationResult ;
+        sh:focusNode ?focusNode ;
+        sh:sourceShape ?sourceShape ;
+        sh:sourceConstraintComponent ?sourceConstraintComponent ;
+        sh:resultSeverity ?resultSeverity ;
+    .
+
+    OPTIONAL {{ ?result sh:resultPath ?resultPath . }}
+    OPTIONAL {{ ?result sh:value ?value . }}
+    OPTIONAL {{ ?result sh:sourceConstraint ?sourceConstraint . }}
+    OPTIONAL {{ ?result sh:resultMessage ?resultMessage . }}
+}}
+");
+
+                new SparqlCsvWriter().Save(results, writer);
+            }),
         };
     }
 }
