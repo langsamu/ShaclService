7f12411e74c7d9dcafaf74ecb7324c3154107dab
Samu Lang <langsamu@hotmail.com>
2025-01-10 17:12:32 +0100

    Reduce scope

--- ShaclService/Program.cs
+++ ShaclService/Program.cs
@@ -7,7 +7,7 @@ public class Program
 {
     public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => webBuilder
             .UseStartup<Startup>());
 }
